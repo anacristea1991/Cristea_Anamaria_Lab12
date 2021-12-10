b5bab2052b588c7722fa512c2a4373aa69fa3dc7
anacristea1991 <92662717+anacristea1991@users.noreply.github.com>
2021-12-10 19:04:24 +0200

    Add project files.

---
+++ Cristea_Anamaria_Lab12/Cristea_Anamaria_Lab12/Data/IRestService.cs
@@ -0,0 +1,15 @@
+﻿using Cristea_Anamaria_Lab12.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cristea_Anamaria_Lab12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
